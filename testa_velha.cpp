445656b1dfd4a50b1540ed685014ac672e9ab0af
elyeladht <elyel.adht@gmail.com>
2026-09-16 09:41:19 -0300

    teste 1

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -11,13 +11,17 @@
   
 
 
-TEST_CASE( "Testa velha", "[single-file]" ) {
+TEST_CASE( "Testa coluna", "[single-file]" ) {
 	int teste1[3][3]= {   { 2, 0, 1 }, 
 	                      { 2, 0, 1 },
 						  { 0, 2, 1 }
 					  };
     REQUIRE( VerificaVelha(teste1) == 1 );
 
-
+    int teste2[3][3]= {   { 2, 1, 1 }, 
+	                      { 2, 0, 2 },
+						  { 2, 1, 1 }
+					  };
+    REQUIRE( VerificaVelha(teste2) == 2 );
 } 
  

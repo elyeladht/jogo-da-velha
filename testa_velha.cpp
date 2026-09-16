86a46a95ced43c82744c405dc8720a69e1d039f7
elyeladht <elyel.adht@gmail.com>
2026-09-16 12:39:26 -0300

    Erro Teste andamento

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -80,3 +80,37 @@ TEST_CASE("Testa diagonais", "[single-file]") {
   };
   REQUIRE(VerificaVelha(teste2) == 1);
 }
+
+TEST_CASE("Testa andamento", "[single-file]") {
+  int teste1[3][3] = {
+    { 2, 0, 2 },
+    { 1, 2, 1 },
+    { 1, 1, 0 }
+  };
+  REQUIRE(VerificaVelha(teste1) == -1);
+
+  int teste2[3][3] = {
+    { 0, 1, 0 },
+    { 2, 0, 2 },
+    { 0, 0, 0 }
+  };
+  REQUIRE(VerificaVelha(teste2) == -1);
+}
+
+/*
+TEST_CASE("Testa empate", "[single-file]") {
+  int teste1[3][3] = {
+    { 2, 1, 1 },
+    { 1, 1, 2 },
+    { 2, 2, 1 }
+  };
+  REQUIRE(VerificaVelha(teste1) == 0);
+
+  int teste2[3][3] = {
+    { 1, 2, 2 },
+    { 2, 2, 1 },
+    { 1, 1, 2 }
+  };
+  REQUIRE(VerificaVelha(teste2) == 0);
+}
+*/

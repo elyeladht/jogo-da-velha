5e43b5f819d7b0230553e88baba6e52e3678408f
elyeladht <elyel.adht@gmail.com>
2026-09-16 12:30:01 -0300

    Refatorando Teste invalido

--- velha.cpp
+++ velha.cpp
@@ -13,48 +13,86 @@
  *  Descrever o que a funcao faz
  */ 
 
+// Protótipo da Função auxiliar
+int GanhadorVelha(int velha[3][3]);
+
+// Função principal de verificação
 int VerificaVelha(int velha[3][3]) {
-  // Verificando jogo inválido
+  // Verificando jogo inválido (1) (diferença de peças)
   int o = 0, x = 0;
-  for (int i = 0; i < 3; ++i)
-  {
-    for (int j = 0; j < 3; ++j)
-    {
-      if(velha[i][j] == 1) {
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      if (velha[i][j] == 1) {
         x++;
-      }
-      else if(velha[i][j] == 2) {
+      } else if (velha[i][j] == 2) {
         o++;
       }
     }
   }
 
-  if (abs(x - o) > 1) {
+  if (std::abs(x - o) > 1) {
     return -2;
   }
+
+  // Passando a matriz para a função auxiliar
+  // Chama para determinar o vencedor ou se teve jogo inválido
+  return GanhadorVelha(velha);
+}
+
+int GanhadorVelha(int velha[3][3]) {
+  // Conta a
+  int xGanhou = 0, oGanhou = 0;
 
   // Verificador de colunas
   for (int j = 0; j < 3; j++) {
     if (velha[0][j] == velha[1][j] && velha[1][j] == velha[2][j] && velha[0][j] != 0) {
-      return velha[0][j];  // Corresponde ao ganhador
+      if (velha[0][j] == 1) {
+        xGanhou++;
+      } else if (velha[0][j] == 2) {
+        oGanhou++;
+      }
     }
   }
 
   // Verificador de linhas
   for (int i = 0; i < 3; i++) {
     if (velha[i][0] == velha[i][1] && velha[i][1] == velha[i][2] && velha[i][0] != 0) {
-      return velha[i][0];  // Corresponde ao ganhador
+      if (velha[i][0] == 1) {
+        xGanhou++;
+      } else if (velha[i][0] == 2) {
+        oGanhou++;
+      }
     }
   }
 
-  // Verificando as diagonais
-  if (velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2]) {
-    return velha[0][0];
+  // Diagonal principal (garantindo que não seja vazia)
+  if (velha[0][0] == velha[1][1] && velha[1][1] == velha[2][2] && velha[1][1] != 0) {
+    if (velha[0][0] == 1) {
+      xGanhou++;
+    } else if (velha[0][0] == 2) {
+      oGanhou++;
+    }
   }
 
-  if (velha[0][2] == velha[1][1] && velha[1][1] == velha[2][0]) {
-    return velha[0][2];
+  // Diagonal secundaria (garantindo que não seja vazia)
+  if (velha[0][2] == velha[1][1] && velha[1][1] == velha[2][0] && velha[1][1] != 0) {
+    if (velha[0][2] == 1) {
+      xGanhou++;
+    } else if (velha[0][2] == 2) {
+      oGanhou++;
+    }
   }
 
-  return 0;  // Nenhum vencedor encontrado ainda
+  // Se ambos ganharam ao mesmo tempo, é jogo inválido
+  if (xGanhou > 0 && oGanhou > 0) {
+    return -2;
+  }
+
+  if (xGanhou > oGanhou) {
+    return 1;
+  } else if (oGanhou > xGanhou) {
+    return 2;
+  } else {
+    return 0; // Nenhum vencedor
+  }
 }
--- velha.hpp
+++ velha.hpp
@@ -1,3 +1,5 @@
 int VerificaVelha( int velha[3][3] ); 
+int GanhadorVelha( int velha[3][3] ); 
+
  
 
